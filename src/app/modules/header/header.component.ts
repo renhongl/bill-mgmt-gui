1d0df2c4023af1fc11deff099c621051deb2ae97
renhongl <1075220132@qq.com>
2020-03-21 18:24:29 +0800

    update

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -54,12 +54,22 @@ export class HeaderComponent implements OnInit, AfterViewInit {
 
   darkTheme() {
     document.documentElement.style.setProperty('--bg', '#18191f');
+    document.documentElement.style.setProperty('--bgHover', '#141419');
+    document.documentElement.style.setProperty('--default', '#b1b1b1');
     document.documentElement.style.setProperty('--shadow', '3px 3px 5px #020202, -3px -3px 5px #23252d, -3px -3px 3px #23252d');
+    document.documentElement.style.setProperty('--insetShadow', 'inset 3px 3px 5px #020202, inset -3px -3px 5px #23252d, inset -3px -3px 3px #23252d');
+    document.documentElement.style.setProperty('--textShadow', '0 0 2px var(--primary)');
+    document.documentElement.style.setProperty('--inputShadow', 'inset 3px 3px 5px var(--primary), inset -1px -1px 3px var(--primary), inset -3px -3px 3px #23252d');
   }
 
   lightTheme() {
-    document.documentElement.style.setProperty('--bg', '#fff');
-    document.documentElement.style.setProperty('--shadow', '3px 3px 5px #02020236, -3px -3px 5px #a2a2a23b, -3px -3px 3px #efefef30');
+    document.documentElement.style.setProperty('--bg', '#f3f0f1');
+    document.documentElement.style.setProperty('--bgHover', '#f3f0f1');
+    document.documentElement.style.setProperty('--default', '#b1b1b1');
+    document.documentElement.style.setProperty('--shadow', '-6px -6px 10px rgba(255, 255, 255, 0.8), 6px 6px 10px rgba(0, 0, 0, 0.2)');
+    document.documentElement.style.setProperty('--insetShadow', 'inset -4px -4px 8px rgba(255, 255, 255, 0.5), inset 8px 8px 16px rgba(0, 0, 0, 0.1)');
+    document.documentElement.style.setProperty('--textShadow', '0 0 2px var(--primary)');
+    document.documentElement.style.setProperty('--inputShadow', 'none');
   }
 
 }
